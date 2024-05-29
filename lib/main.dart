ad7e3418d8d7298352af4559200f49606a094faa
Sihle Kambula <kambulasihle@gamil.com>
2024-05-29 22:21:38 +0200

    linked app to firbase and enable Google sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+//screens
 import 'package:vi/auth/auth/screen/auth_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Vi());
 }
 
